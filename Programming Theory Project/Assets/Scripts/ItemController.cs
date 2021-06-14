1b7ac87401309fb9c26f9b12f2ecfabff2a76d6a
Nasibullin Timur <drmorriarti@users.noreply.github.com>
2021-06-15 01:38:20 +0300

    Screenshots

--- Programming Theory Project/Assets/Scripts/ItemController.cs
+++ Programming Theory Project/Assets/Scripts/ItemController.cs
@@ -5,18 +5,6 @@ public abstract class ItemController : MonoBehaviour
 {
     private bool _isActive = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collision");
